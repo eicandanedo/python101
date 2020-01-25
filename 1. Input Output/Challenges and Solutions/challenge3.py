0cd49ab41e6991f8a9cfcfc144f3a1076c684535
fcpsace <55012003+fcpsace@users.noreply.github.com>
2020-01-24 21:13:27 -0500

    Update challenge3.py

--- 1. Input Output/Challenges and Solutions/challenge3.py
+++ 1. Input Output/Challenges and Solutions/challenge3.py
@@ -1,12 +1,11 @@
 #ask user for length of square and convert to integer
-
 side = int(input("How long is the side of the square? "))
 
+#caluclate perimeter and area
 perimeter = side + side + side + side
-p = side * 4
 area  = side * side
 
+#output results to console and quit
 print(f"The perimeter is {perimeter} feet")
-print(f"The area is {area} feet)
-
-input()
+print(f"The area is {area} feet squared")
+input('press any key to quit')
