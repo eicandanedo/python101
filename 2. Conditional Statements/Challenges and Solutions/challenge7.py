6397e49c5940794a561b25e5cb23385c34163087
fcpsace <55012003+fcpsace@users.noreply.github.com>
2021-04-22 19:29:21 -0400

    Update and rename challenge8.py to challenge7.py

--- 2. Conditional Statements/Challenges and Solutions/challenge7.py
+++ 2. Conditional Statements/Challenges and Solutions/challenge7.py
@@ -4,7 +4,7 @@ subtotal = int(input("What is your subtotal price: "))
 prime = input("Are you a prime member? (y/n) ")
 
 
-if prime == "y" or subtotal > 50:
+if prime == "y" or subtotal >= 50:
     shipping = 0
 else:
     shipping = 8
