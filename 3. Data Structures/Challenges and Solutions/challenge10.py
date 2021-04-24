5158cf8982a5a3110673e84a0d79c6d8b5454f9f
fcpsace <55012003+fcpsace@users.noreply.github.com>
2021-04-24 06:52:35 -0400

    Update challenge10.py

--- 3. Data Structures/Challenges and Solutions/challenge10.py
+++ 3. Data Structures/Challenges and Solutions/challenge10.py
@@ -1,4 +1,3 @@
-
 #create dictionary with Hello in each language
 translator = {"French":"Bonjour",
               "Spanish":"Hola",
@@ -7,25 +6,6 @@ translator = {"French":"Bonjour",
               "Indian": "Namaste"
               }
 
-#if user enters a language in dictionary, translate the word. Otherwise, let them add it
-#if user enters quit, exit program
-while True:
-    selection = input("Select your language? ").title()
-    if selection == "Quit":
-        break
-    
-    elif selection in translator:
-        print(translator.get(selection))
-    
-    elif selection not in translator:
-        print("Thats a new language. Lets add it")
-        new_language = input(f"What does 'Hello' mean in {selection}: ")
-        translator[selection] = new_language
-        
-        #alternative way of adding a language
-        #translator.update({selection:new_language})
-        
-        
+language_selection = input("Select a language: ")
 
-
- 
+print(translator.get(language_selection))
